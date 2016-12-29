c6cb9cf5b5195ab68d2b3fce7cff81e74790b82f
engineer-pearl <theadavis101@gmail.com>
2016-12-29 15:29:11 -0800

    Added file to link into web directory.
    Next step: set up includes.

---
+++ IMPORTANT.php
@@ -0,0 +1,18 @@
+<?php
+/*       NOTICE
+FAILURE TO FOLLOW INSTRUCTIONS BELOW MIGHT DAMAGE THE PROGRAM
+
+Instructions:
+
+- Place a link to this file in /var/www/html/
+- Name the file based on where you want users to be directed.
+DO NOT PUT A LINK TO OUTNET IN THE WEB DIRECTORY. ONLY A LINK TO THIS FILE SHOULD BE THERE.
+ALL PARTS OF THE PROGRAM WILL BE CONNECTED THROUGH THIS FILE.
+*/
+
+echo "test";
+
+/*
+Maintained by ALD Productions and EngineerPearl0
+*/
+?>
