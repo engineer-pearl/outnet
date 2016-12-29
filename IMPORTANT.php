1af74b55999ea00c5f9fcaa79b8c79b44563311d
engineer-pearl <theadavis101@gmail.com>
2016-12-29 15:39:12 -0800

    IMPORTANT.php is now ready
    IMPORTANT.php ready to be linked into /var/www/html/
    Commented steps for file to work as intended
    
    Must be linked in; does not mater if soft or hard.

--- IMPORTANT.php
+++ IMPORTANT.php
@@ -10,6 +10,15 @@ DO NOT PUT A LINK TO OUTNET IN THE WEB DIRECTORY. ONLY A LINK TO THIS FILE SHOUL
 ALL PARTS OF THE PROGRAM WILL BE CONNECTED THROUGH THIS FILE.
 */
 
+//include files that always need to be there
+    //Database calls needed to maintain logins
+    //Database calls to check all groups a user is in
+    //Config variables
+
+//Check what page should be shown in URL
+//Hand control to page's PHP
+
+
 echo "test";
 
 /*
