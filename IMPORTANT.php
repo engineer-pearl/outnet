372cb9becdcc23f6fc0ee26cd48161ebd0830b58
engineer-pearl <theadavis101@gmail.com>
2016-12-29 16:57:03 -0800

    Further preperation for 6:00 test
    [I keep thinking I'm done for then but I keep being wrong. Sorry.]
    
    [Wait who am I apologising to, no one is actually going to read this and get my updates!!! D:]
    
    Point is, now I am taking variables from the URL, so it is closer to being ready for database intigration.
    
    Mental note: first page will become unavalible if database goes down... need to prepare apology message or landing page to handle such a situation.
        Okay maybe that became more than mental...

--- IMPORTANT.php
+++ IMPORTANT.php
@@ -15,15 +15,39 @@ $home_dir = "../../../home/OutNet";
 
 //include files that always need to be there
 include "${home_dir}/lib/mysql.php"; //database calls; to include login calls, web-calls and user-setting calls. Should also handle error handeling.
-
 include "${home_dir}/config.php"; //Config variables //no values avalible
 
 //Check what page should be shown in URL
+
+if(isset($_GET['Gr']))
+{
+    $group = htmlspecialchars($_GET['Gr']); //group
+}
+else
+{
+    $group = "Public";
+}
+if(isset($_GET['Pg']))
+{
+    $page = htmlspecialchars($_GET['Pg']); //page name
+}
+else
+{
+    $page = "Home";
+}
 //Hand control to page's PHP
+?>
 
+<p>If this page is displaying in your browser, then the server system has been successfully installed. Stand by for functionality.</p>
+<p>
+<tr>
+    <td> Your current group is set to <?= $group ?>. </td>
+    <td> Your current page is set to <?= $page ?>.  </td>
+</tr>
+</p>
+<p> Add ?&Gr=Group&Pg=Page to the url to change these values. </p>
 
-echo "If this page is displaying in your browser, then the server system has been successfully installed. Stand by for functionality.";
-
+<?php
 /*
 Maintained by ALD Productions and EngineerPearl0
 */
