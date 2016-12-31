234470c1d75da321df3d0e6f2b21f9ce1295fb8c
engineer-pearl <theadavis101@gmail.com>
2016-12-31 15:23:15 -0800

    Corrected error.
    Updated leftbar.php.

---
+++ Groups/Public/panels/leftbar.php
@@ -0,0 +1,28 @@
+<?php
+    /*
+        Chat
+    */
+?>
+<html>
+    <div class="sidenotehead">
+            ADMIN'S NOTES:
+        <div class="sidenotebody">
+            <p><span style="color: grey;">Chat put on back-order.</span></p>
+            <p><span style="color: green;">Content changes without forcing new template.</span></p>
+            <p><span style="color: red;">Content only avalible by knowing URL, must add in database search.</span></p>
+            <p><span style="color: red;">Navigation menues not yet written.</span></p>
+            <p style="color: green;">Database structure written</p>
+            <p style="color: red;">Encryption not yet avalible. DO NOT RE-USE IMPORTANT PASSWORDS ON THIS SITE.</p>
+            <p style="color: green;">Using PDO for MySQL database calls</span></p>
+            <p style="color: green;">Can use content from URL (Use "?note=" to change the header</p>
+            <p style="color: green;">Content is mostly located in protected area.</p>
+            <p style = "color: red;">Multiple security risks acknowledged due to lack of SQL database calls and call controls</p>
+        </div>
+<?php
+
+    /*
+        Read announcements soon lists for other allowed groups
+    */
+?>
+    </div>
+</html>
