bdcf21be1aed8dea9bcc24b9d8c779cb8cf30965
engineer-pearl <theadavis101@gmail.com>
2017-01-23 17:59:05 -0800

    Non-Actve Structure Updates
    Removed contents of folder Public
    
    Changed structure of login.php to hold the interactive portions of the login/logout/etc. process.

--- lib/Head/login.php
+++ lib/Head/login.php
@@ -1,12 +1,49 @@
-<?php 
-//contains the look of the page
+<?php
 function header_login()
 {
-    
+    /*something*/
+    login($username, $pass);
+    //refresh;
 }
 function page_login()
 {
-    
+    /*something*/;
+    if (login($username, $pass))
+    {;
+    /*
+     if (target in url) 
+     {
+        goto target;
+     }
+     else if (target in cookie)
+     {
+        go to target;
+     }
+     else
+     {
+        go to public page;
+     }
+     */;
+    }
+    else
+    {
+        //Needs to store failed login IPs
+        //Needs to store IPs somewhere... IPs MUST be encrypted
+        //Needs to store failed logins
+        echo "login failed."
+    }
 }
-
+function LOCKDOWN()
+{
+    if ("$confirm == y")
+    {
+        //get current user id
+        //take new password info
+        //take new user info
+        updateprofile();
+        //disallow any allowed apps
+        //change "login key"
+        
+    }
+}
 ?>
